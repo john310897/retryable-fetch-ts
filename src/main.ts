86dea4b6b918fc69d691e1a1591c793c2401abbd
John J <76699031+john310897@users.noreply.github.com>
2025-07-08 11:15:17 +0000

    clean up package main file

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,21 @@
 import axios from "axios"
 async function fetchApi(api: string, init: {}, method: string = 'GET') {
-    const response = await axios.get(api, { withCredentials: true }).catch(err => err)
-    return response
+    return await axios.get(api, { withCredentials: true }).catch(err => err)
 }
 
 const intervalPromise = (api: string, init: object, interval: number, timeout?: number) => {
-    console.log("in the interval promise.................")
     return new Promise((resolve, reject) => {
         let response: any;
         let endapi: boolean = false
-        console.log('changes in the main wile through package imptoves')
         if (timeout)
             setTimeout(() => {
                 endapi = true
             }, timeout)
         const apiInterval = setInterval(async () => {
             response = await fetchApi(api, init)
-            console.log("--------------------------",response)
             if (response?.status === 200 || (timeout && endapi === true)) {
                 clearInterval(apiInterval)
-                if (response.status===200) resolve(response?.data)
+                if (response.status === 200) resolve(response?.data)
                 else reject(response)
                 return
             }
@@ -29,21 +25,16 @@ const intervalPromise = (api: string, init: object, interval: number, timeout?: 
 
 async function retryFetch(api: string, init: {}, interval: number = 0, timeout: number = 0) {
     // stage--1 direct api call without interval and timeout
-    if (!interval && !timeout) {
-        const response = await fetchApi(api, init)
-        return response
-    }
+    if (!interval && !timeout)
+        return await fetchApi(api, init)
 
     // stage--2 run api on failure for the given interval
-    if (interval && !timeout) {
+    if (interval && !timeout)
         return await intervalPromise(api, init, interval)
-    }
 
     // stage--3 run api on failure for the given interval and till the given timeout
-    if (interval && timeout) {
-        console.log("in stage -----------------------------3")
+    if (interval && timeout)
         return await intervalPromise(api, init, interval, timeout)
-    }
 }
 
 export { retryFetch }
